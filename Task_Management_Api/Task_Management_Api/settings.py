0e3077c12d67c54b0c45b6b1dd04a0415a0e49d5
Ecode2 <ecode5814@gmail.com>
2024-10-18 21:32:25 +0100

    made configurations to settings for deployment

--- Task_Management_Api/Task_Management_Api/settings.py
+++ Task_Management_Api/Task_Management_Api/settings.py
@@ -44,6 +44,8 @@ if config("DEBUG", default=False, cast=bool) == False:
     
     ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="", cast=Csv())
 
+    #CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", default="", cast=Csv())
+
     SECURE_SSL_REDIRECT=False
 
     SECURE_HSTS_INCLUDE_SUBDOMAINS=True
@@ -148,6 +150,7 @@ else:
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
+        'rest_framework.authentication.SessionAuthentication',
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
